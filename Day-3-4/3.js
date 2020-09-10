1c6d1b6aa4a5e418cb6822e639d02083bee2f674
Aravindreddy Mokireddy <49472393+aravindreddymokireddy@users.noreply.github.com>
2020-09-10 13:11:10 +0530

    Add files via upload

---
+++ Day-3-4/3.js
@@ -0,0 +1,32 @@
+let developers = [{
+        name: 'John',
+        age: 25,
+        country: "USA",
+        hobbies: ["Reading Books", "Playing Games", "Listening Songs"]
+    },
+
+    {
+        name: 'Rahul',
+        age: 38,
+        country: "India",
+        hobbies: ["Writing Stories", "Developing Games", "Dance"]
+    },
+
+    {
+        name: 'Ramu',
+        age: 32,
+        country: "India",
+        hobbies: ["Programming", "Teaching", "Singing"]
+    },
+
+    {
+        name: 'Charlie',
+        age: 28,
+        country: "Brazil",
+        hobbies: ["Reading Books", "Writing Stories", "Singing"]
+    }
+];
+
+function display() {
+    console.log(developers);
+}
